20c3fc00ee039d8ee83fbc5072decd5c6f05de0d
Anderson Soriano <19699525+andersori@users.noreply.github.com>
2020-10-26 10:27:56 -0300

    remove notnull

--- src/main/java/io/github/andersori/spreadsheet/Pair.java
+++ src/main/java/io/github/andersori/spreadsheet/Pair.java
@@ -2,15 +2,14 @@ package io.github.andersori.spreadsheet;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.ToString;
 
 @Getter
 @ToString
 @AllArgsConstructor
 public final class Pair<S, T> {
-	private final @NonNull S first;
-	private final @NonNull T second;
+	private final S first;
+	private final T second;
 	
 	public static <S, T> Pair<S, T> of(S first, T second) {
 		return new Pair<>(first, second);
