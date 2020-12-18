c7feea8eee71b1643b9aa4d92b9fb3c424679a55
Anderson Soriano <19699525+andersori@users.noreply.github.com>
2020-12-18 18:03:49 -0300

    Other options to read

--- src/main/java/io/github/andersori/spreadsheet/txt/TXTUtils.java
+++ src/main/java/io/github/andersori/spreadsheet/txt/TXTUtils.java
@@ -1,67 +1,86 @@
 package io.github.andersori.spreadsheet.txt;
 
-import java.io.BufferedWriter;
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.SequenceInputStream;
 import java.nio.charset.Charset;
-import org.apache.commons.io.FilenameUtils;
+import java.util.function.Function;
 import org.apache.tika.io.IOUtils;
+import org.apache.tika.parser.txt.TXTParser;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.http.HttpStatus;
+import org.springframework.core.io.support.EncodedResource;
 import org.springframework.http.codec.multipart.FilePart;
-import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 public class TXTUtils {
 
-  public static Mono<Resource> replace_UTF_8(
-      FilePart file, String oldCharacter, String newCharacter) {
-    return replace(file, oldCharacter, newCharacter, Charset.forName("UTF-8"));
-  }
-
-  public static Mono<Resource> replace_ISO_8859_1(
-      FilePart file, String oldCharacter, String newCharacter) {
-    return replace(file, oldCharacter, newCharacter, Charset.forName("ISO-8859-1"));
-  }
-
-  public static Mono<Resource> replace(
-      FilePart file, String oldCharacter, String newCharacter, Charset cs) {
-    if (!(FilenameUtils.getExtension(file.filename()).equalsIgnoreCase("txt"))) {
-      return Mono.error(
-          new ResponseStatusException(HttpStatus.BAD_REQUEST, "Só é permitido a leitura de TXT."));
-    }
+  public static Mono<Resource> decode(FilePart file, Charset current, Charset target) {
     return file.content()
         .map(DataBuffer::asInputStream)
         .reduce(SequenceInputStream::new)
         .flatMap(
             input ->
+                Mono.fromCallable(() -> new String(input.readAllBytes(), current).getBytes(target)))
+        .doOnNext(bytes -> System.out.println(new String(bytes)))
+        .flatMap(
+            bytes ->
                 Mono.fromCallable(
                     () ->
-                        IOUtils.readLines(input, cs.name()).stream()
-                            .map(line -> line.replace(oldCharacter, newCharacter))
-                            .reduce(
-                                "",
-                                (a, b) -> {
-                                  if (a.isBlank()) {
-                                    return b;
-                                  }
-                                  return a + "\n" + b;
-                                })))
+                        new ByteArrayResource(
+                            IOUtils.toByteArray(
+                                new EncodedResource(new ByteArrayResource(bytes), target)
+                                    .getReader()))));
+  }
+
+  public static Mono<Resource> decode(Resource file, Charset current, Charset target) {
+	  
+	  new TXTParser(encodingDetector)
+    return Mono.fromCallable(() -> file.getInputStream())
         .flatMap(
-            text ->
+            input ->
+                Mono.fromCallable(() -> new String(input.readAllBytes(), current).getBytes(target)))
+        .doOnNext(bytes -> System.out.println(new String(bytes)))
+        .flatMap(
+            bytes ->
                 Mono.fromCallable(
-                    () -> {
-                      ByteArrayOutputStream inMemory = new ByteArrayOutputStream();
-                      BufferedWriter inMemoryStream =
-                          new BufferedWriter(new OutputStreamWriter(inMemory, cs));
+                    () ->
+                        new ByteArrayResource(
+                            IOUtils.toByteArray(
+                                new EncodedResource(new ByteArrayResource(bytes), target)
+                                    .getReader()))));
+  }
 
-                      inMemoryStream.write(new String(text.getBytes(), cs));
-                      inMemoryStream.flush();
+  private static Function<InputStream, Mono<Resource>> replace(
+      String oldCharacter, String newCharacter) {
+    return input ->
+        Mono.fromCallable(
+                () ->
+                    new BufferedReader(new InputStreamReader(input))
+                        .lines()
+                        .map(line -> line.replace(oldCharacter, newCharacter))
+                        .reduce(
+                            "",
+                            (a, b) -> {
+                              if (a.isBlank()) {
+                                return b;
+                              }
+                              return a + "\n" + b;
+                            }))
+            .flatMap(text -> Mono.fromCallable(() -> new ByteArrayResource(text.getBytes())));
+  }
 
-                      return new ByteArrayResource(inMemory.toByteArray());
-                    }));
+  public static Mono<Resource> replace(FilePart file, String oldCharacter, String newCharacter) {
+    return file.content()
+        .map(DataBuffer::asInputStream)
+        .reduce(SequenceInputStream::new)
+        .flatMap(replace(oldCharacter, newCharacter));
+  }
+
+  public static Mono<Resource> replace(Resource file, String oldCharacter, String newCharacter) {
+    return Mono.fromCallable(() -> file.getInputStream())
+        .flatMap(replace(oldCharacter, newCharacter));
   }
 }
