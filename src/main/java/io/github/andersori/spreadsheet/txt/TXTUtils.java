0adc19a66c8a7658f9eb0918e9971d06c8cf36e1
Anderson Soriano <19699525+andersori@users.noreply.github.com>
2020-12-21 10:39:38 -0300

    decode method removed

--- src/main/java/io/github/andersori/spreadsheet/txt/TXTUtils.java
+++ src/main/java/io/github/andersori/spreadsheet/txt/TXTUtils.java
@@ -4,54 +4,16 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.SequenceInputStream;
-import java.nio.charset.Charset;
 import java.util.function.Function;
-import org.apache.tika.io.IOUtils;
-import org.apache.tika.parser.txt.TXTParser;
+
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.support.EncodedResource;
 import org.springframework.http.codec.multipart.FilePart;
+
 import reactor.core.publisher.Mono;
 
 public class TXTUtils {
-
-  public static Mono<Resource> decode(FilePart file, Charset current, Charset target) {
-    return file.content()
-        .map(DataBuffer::asInputStream)
-        .reduce(SequenceInputStream::new)
-        .flatMap(
-            input ->
-                Mono.fromCallable(() -> new String(input.readAllBytes(), current).getBytes(target)))
-        .doOnNext(bytes -> System.out.println(new String(bytes)))
-        .flatMap(
-            bytes ->
-                Mono.fromCallable(
-                    () ->
-                        new ByteArrayResource(
-                            IOUtils.toByteArray(
-                                new EncodedResource(new ByteArrayResource(bytes), target)
-                                    .getReader()))));
-  }
-
-  public static Mono<Resource> decode(Resource file, Charset current, Charset target) {
-	  
-	  new TXTParser(encodingDetector)
-    return Mono.fromCallable(() -> file.getInputStream())
-        .flatMap(
-            input ->
-                Mono.fromCallable(() -> new String(input.readAllBytes(), current).getBytes(target)))
-        .doOnNext(bytes -> System.out.println(new String(bytes)))
-        .flatMap(
-            bytes ->
-                Mono.fromCallable(
-                    () ->
-                        new ByteArrayResource(
-                            IOUtils.toByteArray(
-                                new EncodedResource(new ByteArrayResource(bytes), target)
-                                    .getReader()))));
-  }
 
   private static Function<InputStream, Mono<Resource>> replace(
       String oldCharacter, String newCharacter) {
